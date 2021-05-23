c5e8c026a01dd53cbe42c96896cba6f74f68de8a
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-23 23:50:27 +0530

    remove disclaimer

--- src/pages/Viewers/HtmlViewer.tsx
+++ src/pages/Viewers/HtmlViewer.tsx
@@ -6,19 +6,6 @@ export default function HtmlViewer() {
   const hash = window.location.hash.substr(1);
 
   const ungzipped = useUnGzip(hash);
-  const [hasAgreed, setAgreed] = useState(false);
-
-  if (!hasAgreed)
-    return (
-      <div class={confDialog}>
-        <div>Are you sure you want to proceed?</div>
-        <div>
-          The contents of this page can be dangerous, they're not verified, and
-          you should treat it like any dangerous {"<iframe>"}
-        </div>
-        <button onClick={() => setAgreed(true)}>I Understand</button>
-      </div>
-    );
   if (!hash.length) return <div>Nothing here...</div>;
   if (!ungzipped) return <div>Loading...</div>;
   return (
